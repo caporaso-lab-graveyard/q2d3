320fc505565f3086ce24d1cdca8d0bdbe1dd1f83
Greg Caporaso <gregcaporaso@gmail.com>
2016-03-25 06:45:29 -0700

    initial commit
    this code was ported from the QIIME 2 interfaces module. Most of this was
    written for a demo by @ebolyen, @jairideout and myself.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# q2ui-web
-Web interface for QIIME 2
+# q2ui-q2d3
+
+This is a prototype that illustrates how to use the QIIME 2 SDK to build an interface. This is based on the earlier [q2d2](https://github.com/gregcaporaso/q2d2) prototype. As this is a prototype, you shouldn't rely on any of the code in this package.
+
+For a demo of this interface, see the [q2d3-interface-demo](https://github.com/gregcaporaso/q2d3-interface-demo) repository.
---
+++ setup.py
@@ -0,0 +1,20 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016--, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+# ----------------------------------------------------------------------------
+
+import glob
+from setuptools import find_packages, setup
+
+setup(
+    name='q2ui-q2d3',
+    version='0.0.1.dev0',
+    packages=find_packages(),
+    scripts=glob.glob('scripts/*'),
+    install_requires=['click', 'python-frontmatter', 'pyyaml',
+                      'qiime >= 2.0.0', 'tornado'],
+    package_data={'q2d3': ['static/*']}
+)
